8c23071364fe8fcc9f558671eb6275a716698f84
Bowen-Jiang <im.bowen.jiang@gmail.com>
2023-02-15 00:04:22 +1100

    add dialogue turning off feature

--- Assets/Hero Knight - Pixel Art/Demo/DialogueSystem.cs
+++ Assets/Hero Knight - Pixel Art/Demo/DialogueSystem.cs
@@ -10,6 +10,7 @@ public class DialogueSystem : MonoBehaviour
     private int index;
     public float typeSpeed;
     public GameObject Background; 
+    private bool isClosedDialogue = false;
 
     // Start is called before the first frame update
     void Start()
@@ -17,14 +18,17 @@ public class DialogueSystem : MonoBehaviour
         StartCoroutine(Type());
         Background.SetActive(false);
     }
-
+ 
     IEnumerator Type() 
     {
         foreach(char letter in sentenses[index].ToCharArray())
         {
             textDisplay.text += letter;
             yield return new WaitForSeconds(typeSpeed);
+            
         }
+        yield return new WaitForSeconds(5);
+        CloseDialogue();
     }
 
     public void DisplayDialog(int dialogueIndex) 
@@ -32,15 +36,23 @@ public class DialogueSystem : MonoBehaviour
         // trigger specific dialogue when event happens
         // index 0 is empty string
         textDisplay.text = "";
-        Debug.Log("TriggerDisplayDialog" + sentenses[dialogueIndex]);
-        if (dialogueIndex <= sentenses.Length - 1)
+        Debug.Log("TriggerDisplayDialog" + dialogueIndex);
+        if (dialogueIndex <= sentenses.Length - 1 && dialogueIndex > 0)
         {
             index = dialogueIndex;
             Background.SetActive(true);
+            isClosedDialogue = false;
             StartCoroutine(Type());
         } else {
             Background.SetActive(false);
             index = 0;
+        }
+    }
+    void CloseDialogue() 
+    {
+        if (!isClosedDialogue) {
+            DisplayDialog(0);
+            isClosedDialogue = true;
         }
     }
 }

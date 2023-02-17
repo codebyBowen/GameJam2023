a65c6cf6aca160becc8528715fe9a978405c400c
Bowen-Jiang <im.bowen.jiang@gmail.com>
2023-02-17 22:31:57 +1100

    Add flash script, modify character scripts to show flash effect
    Update penguin prefab

--- Assets/Scripts/CombatCharacter.cs
+++ Assets/Scripts/CombatCharacter.cs
@@ -1,10 +1,19 @@
 using UnityEngine;
+using BarthaSzabolcs.Tutorial_SpriteFlash;
 
 public class CombatCharacter : MonoBehaviour {
   [SerializeField] public Health health;
   public AttackProp attProp;
 
+  protected SimpleFlash flashEffect;
+
+  void Start() {
+    flashEffect = GetComponent<SimpleFlash>();
+  }
+
   public virtual void takeDamage(AttackProp ap) {
+    if (flashEffect) flashEffect.Flash();
+
     health.changeHP(-Damage.CalculateDamage(ap, attProp));
   }
 }
